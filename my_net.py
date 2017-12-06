cf228ee10d81ed8a78fb2b91e5b51f23cf14a2c9
Srikrishna Karanam <srikrishna6891@gmail.com>
2017-12-05 20:19:32 -0500

    //

--- my_net.py
+++ my_net.py
@@ -18,94 +18,164 @@ import torch.nn.functional as F
 
 # create dataset
 
-class_label = {"grass" : 0, "ocean" : 1, "redcarpet" : 2, "road" : 3, "wheatfield" : 4}
 
-folder_train = 'data/train'
-folder_test = 'data/test'
+use_cuda = torch.cuda.is_available()
 
-n_sample_from_class = 500
-n_test_sample_from_class = 90
+def scale(x):
+    return (x - np.mean(x)) / np.std(x)
 
-# 120*180*3
-r_size = (64, 64)
-t_size = int(64 * 64 * 3)
+def success_rate(loader, do_print = False):
+    correct = 0
+    total = 0
+    for ep in range(epochs):
+        for data in loader:
+            images, labels = data
+            if use_cuda:
+                outputs = net(Variable(images.cuda()))
+                labels = labels.cuda()
+            else:
+                outputs = net(Variable(images))
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            #print("value:----------")
+            #print(predicted)
+            correct += (predicted == labels).sum()
+    rate =  correct / total
+    if do_print:
+        print('Accuracy of the network on the %d  images: %d %%' % (total, 
+        100 * rate))
+        print(confusion_matrix(predicted.cpu().numpy(), labels.cpu().numpy()))
 
-X_train = np.ndarray((n_sample_from_class * 5, t_size))
-Y_train = np.zeros((n_sample_from_class * 5, 5), dtype=np.uint8)
+    return rate
 
-for label, y in class_label.items():
-    class_path = os.path.join(folder_train, label)
-    all_files = os.listdir(class_path)
-    all_im_files = [f for f in all_files if f.endswith('JPEG')]
-    im_files = random.sample(all_im_files, n_sample_from_class)
+
+def accuracy(out, act):
+    _, predicted = torch.max(out.data, 1)
+    total = act.size(0)
+    correct = (predicted == act).sum()
+    #conf = confusion_matrix(act, predicted)
+    return correct / total
+
+def create_dataset(n_sample_from_class, n_test_sample_from_class, n_valid_percent, batch_size):
+
+    class_label = {"grass" : 0, "ocean" : 1, "redcarpet" : 2, "road" : 3, "wheatfield" : 4}
+
+    folder_train = os.path.join('data', 'train')
+    folder_test = os.path.join('data', 'test')
+
+    # 120*180*3
+    r_size = (64, 64)
+    #t_size = int(64 * 64 * 3)
+
+    n_valid_sample_from_class = int(n_sample_from_class * n_valid_percent)
+    n_train_sample_from_class = n_sample_from_class - n_valid_sample_from_class
     
-    count = 0
-    for imfile in im_files:
-        full_file_path = os.path.join(folder_train, label, imfile)
-        im = cv2.imread(full_file_path)
-        im = cv2.resize(im, r_size, interpolation = cv2.INTER_CUBIC)    
-        if im is None:
-            sys.exit()
-        X_train[y * n_sample_from_class + count] = im.flatten()
-        Y_train[y * n_sample_from_class + count, y] = 1
-        count += 1
+    X_train = np.zeros((n_train_sample_from_class * 5 ,3 * r_size[0] * r_size[1]))
+    Y_train = np.zeros(n_train_sample_from_class * 5, dtype=np.long)
+    
+    X_valid = np.zeros((n_valid_sample_from_class * 5 ,3 * r_size[0] * r_size[1]))
+    Y_valid = np.zeros(n_valid_sample_from_class * 5, dtype=np.long)
 
-# train
-normalizer = preprocessing.StandardScaler().fit(X_train)
-X_train = normalizer.transform(X_train)
-N = n_sample_from_class * 5
-ind_list = [i for i in range(N)]
-random.shuffle(ind_list)
+    for label, y in class_label.items():
+        class_path = os.path.join(folder_train, label)
+        all_files = os.listdir(class_path)
+        all_im_files = [f for f in all_files if f.endswith('JPEG') and not f.startswith('.')]
+        im_files = random.sample(all_im_files, n_sample_from_class)
+        
+        for count,imfile in enumerate(im_files):
+            full_file_path = os.path.join(folder_train, label, imfile)
+            im = cv2.imread(full_file_path)
+            im = cv2.resize(im, r_size, interpolation = cv2.INTER_CUBIC)    
+            if im is None:
+                sys.exit()
+            if count < n_valid_sample_from_class:
+                X_valid[y * n_valid_sample_from_class + count] = scale(im.flatten()) 
+                Y_valid[y * n_valid_sample_from_class + count] = y
+            else:
+                c = count - n_valid_sample_from_class
+                #print(c, y)
+                #print(y * n_sample_from_class + c)
+                X_train[y * n_train_sample_from_class + c] = scale(im.flatten()) 
 
-X_train = X_train[ind_list]
-Y_train = Y_train[ind_list]
+                Y_train[y * n_train_sample_from_class + c] = y
+                
+
+    # train
+    
+    
+    X_test = np.zeros((n_test_sample_from_class * 5 ,3 * r_size[0] * r_size[1]))
+    Y_true = np.zeros((n_test_sample_from_class * 5), dtype=np.int)
 
 
-X_test = np.ndarray((n_test_sample_from_class * 5, t_size))
-Y_true = np.zeros((n_test_sample_from_class * 5, 5), dtype=np.uint8)
+    for label, y in class_label.items():
+        class_path = os.path.join(folder_test, label)
+        all_files = os.listdir(class_path)
+        all_im_files = [f for f in all_files if f.endswith('JPEG') and not f.startswith('.')]
+        im_files = random.sample(all_im_files, n_test_sample_from_class)
+       
+        for count, imfile in enumerate(im_files):
+            full_file_path = os.path.join(folder_test, label, imfile)
+            im = cv2.imread(full_file_path)
+            im = cv2.resize(im, r_size, interpolation = cv2.INTER_CUBIC)    
+            if im is None:
+                sys.exit()
+            X_test[y * n_test_sample_from_class + count] = scale(im.flatten() )
+
+            Y_true[y * n_test_sample_from_class + count] = y
 
 
-for label, y in class_label.items():
-    class_path = os.path.join(folder_test, label)
-    all_files = os.listdir(class_path)
-    all_im_files = [f for f in all_files if f.endswith('JPEG')]
-    im_files = random.sample(all_im_files, n_test_sample_from_class)
-   
-    count = 0
-    for imfile in im_files:
-        full_file_path = os.path.join(folder_test, label, imfile)
-        im = cv2.imread(full_file_path)
-        im = cv2.resize(im, r_size, interpolation = cv2.INTER_CUBIC)    
-        if im is None:
-            sys.exit()
-        X_test[y * n_test_sample_from_class + count] = im.flatten()
-        Y_true[y * n_test_sample_from_class + count, y] = 1
-        count += 1
+    #X_test = normalizer.transform(X_test)
 
-X_test = normalizer.transform(X_test)
+    X_train = torch.from_numpy(X_train).float()
+    #Y_train = torch.from_numpy(Y_train).float()
+    Y_train = torch.LongTensor(Y_train.tolist())
 
-X_train = Variable(torch.from_numpy(X_train).float())
-Y_train = Variable(torch.from_numpy(Y_train).float())
-#Y_train = torch.LongTensor(Y_train)
+    X_valid = torch.from_numpy(X_valid).float()
+    #Y_train = torch.from_numpy(Y_train).float()
+    Y_valid = torch.LongTensor(Y_valid.tolist())
+    X_test = torch.from_numpy(X_test).float()
+    #Y_true = torch.from_numpy(Y_true).float()
+    Y_true = torch.LongTensor(Y_true.tolist())
 
-X_test = Variable(torch.from_numpy(X_test).float())
-Y_true = Variable(torch.from_numpy(Y_true).float())
+    train = data_utils.TensorDataset(X_train, Y_train)
+    train_loader = data_utils.DataLoader(train, batch_size=batch_size, shuffle=True)
+    
+    valid = data_utils.TensorDataset(X_valid, Y_valid)
+    valid_loader = data_utils.DataLoader(valid , shuffle=False)
 
-'''
-train = data_utils.TensorDataset(X_train, Y_train)
-train_loader = data_utils.DataLoader(train, batch_size=batch_size, shuffle=True)
+    test = data_utils.TensorDataset(X_test, Y_true)
+    test_loader = data_utils.DataLoader(dataset=test, shuffle=True)
 
-test = data_utils.TensorDataset(X_test, Y_true)
-test_loader = torch.utils.data.DataLoader(dataset=test, 
-                                          batch_size=batch_size, 
-                                          shuffle=False)
+    return (train_loader, test_loader, valid_loader) 
 
-'''
 
-N0 = t_size
-N1 = 50
-N2 = 30
+n_sample_from_class = 100
+n_test_sample_from_class = 20
+
+epochs = 10
+batch_size = 50
+learning_rate = 0.0001
+
+n_valid_percent = 0.3
+
+train_loader, test_loader, valid_loader = create_dataset(n_sample_from_class, n_test_sample_from_class, n_valid_percent, batch_size)
+
+r_size = (64, 64)
+
+N0 = 64 * 64 * 3
+N1 = 1000
+N2 = 300
+N3 = 300
 Nout = 5
+
+for data in train_loader:
+    X_valid, Y_valid = data
+    if use_cuda:
+        X_valid, Y_valid = Variable(X_valid.cuda()), Variable(Y_valid.cuda())
+    else:
+        X_valid, Y_valid = Variable(X_valid), Variable(Y_valid)
+
+
 
 class Net(nn.Module):
 
@@ -113,96 +183,72 @@ class Net(nn.Module):
         super(Net, self).__init__()
         self.fc1 = nn.Linear( N0, N1, bias=True)
         self.fc2 = nn.Linear( N1, N2, bias=True)
-        self.fc3 = nn.Linear( N2, Nout, bias=True)
+        self.fc3 = nn.Linear( N2, N3, bias=True)
+        self.fc4 = nn.Linear( N3, Nout, bias=True)
 
     def forward(self, x):
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
-        x = self.fc3(x)
+        x = F.relu(self.fc3(x))
+        x = self.fc4(x)
         return x
 
 #  Create an instance of this network.
 net = Net()
 
+if use_cuda:
+    net.cuda()
+
+
 #  Define the Mean Squared error loss function as the criterion for this network's training
-criterion = nn.MSELoss()
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(net.parameters(), lr=learning_rate)
 
 #  Print a summary of the network.  Notice that this only shows the layers
 print(net)
 
-
-
-n_train = X_train.data.size()[0]
-epochs = 100
-batch_size = 10
-n_batches = int(np.ceil(n_train / batch_size))
-learning_rate = 1e-4
-
-#  Compute an initial loss using all of the validation data.
-#
-#  A couple of notes are important here:
-#  (1) X_valid contains all of the validation input, with each validation
-#      data instance being a row of X_valid
-#  (2) Therefore, pred_Y_valid is a Variable containing the output layer
-#      activations for each of the validation inputs.
-#  (3) This is accomplished through the function call net(X_valid), which in
-#      turn calls the forward method under the hood to figure out the flow of
-#      the data and activations in the network.
-#pred_Y_valid = net(X_valid)
-#valid_loss = criterion(pred_Y_valid, Y_valid)
-#print("Initial loss: %.5f" %valid_loss.data[0])
+t_loss = np.zeros(epochs)
 
 for ep in range(epochs):
     #  Create a random permutation of the indices of the row vectors.
-    indices = torch.randperm(n_train)
     
     #  Run through each mini-batch
-    for b in range(n_batches):
-        #  Use slicing (of the pytorch Variable) to extract the
-        #  indices and then the data instances for the next mini-batch
-        batch_indices = indices[b*batch_size:(b+1)*batch_size]
-        batch_X = X_train[batch_indices]
-        batch_Y = Y_train[batch_indices]
+    #t_rate = np.zeros(epochs)
+    
+    for i, data in enumerate(train_loader, 0):  
+        inputs, labels = data 
         
+        if use_cuda:
+            inputs, labels = Variable(inputs.cuda()), Variable(labels.cuda())
+        else:
+            inputs, labels = Variable(inputs), Variable(labels)
+        optimizer.zero_grad() 
+    
         #  Run the network on each data instance in the minibatch
         #  and then compute the object function value
-        pred_Y = net(batch_X)
-        loss = criterion(pred_Y, batch_Y)
+        outputs = net(inputs)
+        loss = criterion(outputs, labels)
         
-        #  Back-propagate the gradient through the network using the
-        #  implicitly defined backward function, but zero out the
-        #  gradient first.
-        net.zero_grad()
+      
         loss.backward()
-
-        #  Complete the mini-batch by actually updating the parameters.
-        for param in net.parameters():
-            param.data -= learning_rate * param.grad.data
-            
-    #  Print validation loss every 10 epochs
-    #if ep != 0 and ep%10==0:
-    #    pred_Y_valid = net(X_valid)
-    #    valid_loss = criterion(pred_Y_valid, Y_valid)
-    #    print("Epoch %d loss: %.5f" %(ep, valid_loss.data[0]))
-
-#  Compute and print the final training and test loss
-#  function values
-pred_Y_train = net(X_train)
-loss = criterion(pred_Y_train, Y_train)
-print('Final training loss is %.5f' %loss.data[0])
-
-pred_Y_test = net(X_test)
-test_loss = criterion(pred_Y_test, Y_true)
-print("Final test loss: %.5f" %test_loss.data[0])
+        optimizer.step()
+        
+        #t_score = accuracy(outputs, labels.data)
+        #t_rate[i] = t_score
+        
+    
+        
+    pred_Y_valid = net(X_valid)
+    valid_loss = criterion(pred_Y_valid, Y_valid)
+    score = accuracy(pred_Y_valid, Y_valid.data)
+    t_loss[ep] = valid_loss.data[0]
+    
+    
+    print ('\nEpoch [%d/%d]\n-------\n Validation :: Loss: %.4f, Accuracy : %.4f' %(ep+1, epochs, valid_loss.data[0], score))
+    #print ('\nEpoch [%d/%d]\n-------\n Validation ::  Accuracy : %.4f' %(ep+1, epochs, score))
 
 
-def success_rate(pred_Y, Y):
-    _,pred_Y_index = torch.max(pred_Y, 1)
-    _,Y_index = torch.max(Y,1)
-    num_equal = torch.sum(pred_Y_index.data == Y_index.data)
-    num_different = torch.sum(pred_Y_index.data != Y_index.data)
-    rate = num_equal / float(num_equal + num_different)
-    return rate
 
-print('Training success rate:', success_rate(pred_Y_train, Y_train))
-print('Test success rate:', success_rate(pred_Y_test, Y_true))
+
+print('Training success rate: %.2f' % success_rate(train_loader))
+print('Test success rate: %.2f' % success_rate(test_loader, True))
